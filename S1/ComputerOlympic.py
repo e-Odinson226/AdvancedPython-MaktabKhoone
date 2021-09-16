dec342ead7db6eeec9e12aa8245b781e78c1124e
Erfan Zare <erfan.yekezare@gmail.com>
2021-09-16 18:44:50 +0430

    Working all fine save as v0.09

--- S1/ComputerOlympic.py
+++ S1/ComputerOlympic.py
@@ -1,14 +1,34 @@
-class Person:
-    def __init__(self, str):
-        dude = str.split(".")
-        self.sex = dude[0]
-        self.name = dude[1]
-        self.lang = dude[2]
+def getter(str):
+    lst = str.strip().split(".")
+    lst[1] = lst[1].title()
+    return lst
+def calculate(mainList, data):
+    if data[0] == "f":
+        mainList["f"].append( { "name":data[1],
+                                "lang":data[2]} )
+    elif data[0] == "m":
+        mainList["m"].append( { "name":data[1],
+                                "lang":data[2]} )
 
 
-many = int(input())
-persons = []
-for i in range(many):
-    str = input("Str: ")
-    persons.append(Person(str))
+
+persons = { "f":[],
+            "m":[]}
+# many = int(input())
+# for i in range(many):
+#     calculate(persons ,getter(input()) )
+calculate(persons ,getter("m.zaKa.py") )
+calculate(persons ,getter("m.JaDi.c+") )
+calculate(persons ,getter("m.erFa.c#") )
+calculate(persons ,getter("f.sArA.c+") )
+calculate(persons ,getter("f.asEm.c+") )
+
+
+persons["m"].sort( key=lambda d: d["name"] )
+persons["f"].sort( key=lambda d: d["name"] )
+
 print(persons)
+for d in persons:
+    print(d)
+# print(persons["f"])
+# print(persons["m"])
